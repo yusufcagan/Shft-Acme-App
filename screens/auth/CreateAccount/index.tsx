c1fdf45e454b023cb2f8e1541cbd75c69ed63175
Yusuf Çağan <yusufcagan@outlook.com.tr>
2024-09-06 22:58:20 +0300

    feat: create auth folder

---
+++ screens/auth/CreateAccount/index.tsx
@@ -0,0 +1,14 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+const CreateAccount = () => {
+  return (
+    <View className="mt-8 px-2">
+      <Text className="mt-2 text-lg text-black dark:text-white">
+        CreateAccount
+      </Text>
+    </View>
+  );
+};
+
+export default CreateAccount;
---
+++ screens/auth/Login/index.tsx
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+const Login = () => {
+  return (
+    <View className="mt-8 px-2">
+      <Text className="mt-2 text-lg text-black dark:text-white">Login</Text>
+    </View>
+  );
+};
+
+export default Login;
