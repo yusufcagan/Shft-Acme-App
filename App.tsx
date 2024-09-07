236112d39887b2153ce071a4ea0aaf43555b4018
Yusuf Çağan <yusufcagan@outlook.com.tr>
2024-09-07 17:01:12 +0300

    feat: tanstack query provide

--- App.tsx
+++ App.tsx
@@ -13,26 +13,30 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {RootStackParamList} from './RootStackParamList';
 import {CreateAccount} from './screens/auth/CreateAccount';
 import {JobList} from './screens/jobs/JobListing';
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 
 const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 function App(): React.JSX.Element {
   const [isLogin, setIsLogin] = useState<boolean>(false);
+  const queryClient = new QueryClient();
   return (
-    <NavigationContainer>
-      {isLogin ? (
-        <Tab.Navigator>
-          <Tab.Screen name="JobList" component={JobList} />
-        </Tab.Navigator>
-      ) : (
-        <Stack.Navigator screenOptions={{headerShown: false}}>
-          <Stack.Screen name="Login" component={Login} />
-          <Stack.Screen name="CreateAccount" component={CreateAccount} />
-          <Tab.Screen name="JobList" component={JobList} />
-        </Stack.Navigator>
-      )}
-    </NavigationContainer>
+    <QueryClientProvider client={queryClient}>
+      <NavigationContainer>
+        {isLogin ? (
+          <Tab.Navigator>
+            <Tab.Screen name="JobList" component={JobList} />
+          </Tab.Navigator>
+        ) : (
+          <Stack.Navigator screenOptions={{headerShown: false}}>
+            <Stack.Screen name="Login" component={Login} />
+            <Stack.Screen name="CreateAccount" component={CreateAccount} />
+            <Tab.Screen name="JobList" component={JobList} />
+          </Stack.Navigator>
+        )}
+      </NavigationContainer>
+    </QueryClientProvider>
   );
 }
 
