37b79d825fea039a0aaecfa7c2859b7f04a9f671
Yusuf Çağan <yusufcagan@outlook.com.tr>
2024-09-11 00:32:30 +0300

    update: job detail lang

--- screens/jobs/JobDetails/index.tsx
+++ screens/jobs/JobDetails/index.tsx
@@ -8,11 +8,15 @@ import {useApplyToJobById} from '../../../services/queries/useApplyToJobByIdMuta
 import {useGetUser} from '../../../services/queries/useGetUser';
 import {useWithdrawToJobById} from '../../../services/queries/useWithdrawJobByIdMutation';
 import {useQueryClient} from '@tanstack/react-query';
+import '../../../lang/i18n';
+import {useTranslation} from 'react-i18next';
 
 export function JobDetailScreen({
   navigation,
   route,
 }: NativeStackScreenProps<JobStackParamList, 'JobDetailScreen'>) {
+  const {t} = useTranslation();
+
   const {id} = route.params;
   const {data: job} = useGetJobById(id);
 
@@ -46,16 +50,28 @@ export function JobDetailScreen({
         <TouchableOpacity onPress={() => navigation.goBack()}>
           <ArrowLeft size="30" color="#000000" />
         </TouchableOpacity>
-        <Text className="text-xl text-black-900 font-semibold">Job Detail</Text>
+        <Text className="text-xl text-black-900 font-semibold">
+          {t('job_detail_screen.title')}
+        </Text>
         <View className="w-[30px]" />
       </View>
       <View className="bg-gray-300 m-5 rounded-[20px] items-center p-5">
         <Bag2 size="40" color="#000000" />
-        <Text className="text-xl text-black-900 font-bold">Job Name</Text>
-        <Text className="text-[13px] text-black-900 font-semibold mt-2">{`Company: ${job?.companyName}`}</Text>
-        <Text className="text-[13px] text-black-900 font-semibold mt-2">{`Location: ${job?.location}`}</Text>
-        <Text className="text-[13px] text-black-900 font-semibold mt-2">{`Salary: ${job?.salary}$`}</Text>
-        <Text className="text-[15px] text-black-900 font-bold mt-2">{`Keyword:`}</Text>
+        <Text className="text-xl text-black-900 font-bold">
+          {t('job_detail_screen.job_name')}
+        </Text>
+        <Text className="text-[13px] text-black-900 font-semibold mt-2">{`${t(
+          'job_detail_screen.company',
+        )} ${job?.companyName}`}</Text>
+        <Text className="text-[13px] text-black-900 font-semibold mt-2">{`${t(
+          'job_detail_screen.location',
+        )} ${job?.location}`}</Text>
+        <Text className="text-[13px] text-black-900 font-semibold mt-2">{`${t(
+          'job_detail_screen.salary',
+        )} ${job?.salary}$`}</Text>
+        <Text className="text-[15px] text-black-900 font-bold mt-2">{`${t(
+          'job_detail_screen.keywords',
+        )}`}</Text>
         <View className="flex-row flex-wrap mt-2">
           {job?.keywords.map((j, index) => (
             <View
@@ -65,7 +81,9 @@ export function JobDetailScreen({
             </View>
           ))}
         </View>
-        <Text className="text-[15px] text-black-900 font-bold mt-2">{`Job Description`}</Text>
+        <Text className="text-[15px] text-black-900 font-bold mt-2">{`${t(
+          'job_detail_screen.job_description',
+        )}`}</Text>
         <View className="bg-white border-2 border-gray-800 p-2 m-4">
           <Text className="text-[15px] text-black-900 font-bold mt-2">
             {job?.description}
@@ -76,14 +94,16 @@ export function JobDetailScreen({
             onPress={handleWithdrawJob}
             className="bg-white p-3 border-2 rounded-md mb-4 w-2/3 items-center">
             <Text className="text-black text-[17px] font-bold mx-2">
-              Withdraw
+              {t('job_detail_screen.withdraw_button')}
             </Text>
           </TouchableOpacity>
         ) : (
           <TouchableOpacity
             onPress={handleApplyJob}
             className="bg-black p-3 rounded-md mb-4 w-2/3 items-center">
-            <Text className="text-white text-[17px] font-bold mx-2">Apply</Text>
+            <Text className="text-white text-[17px] font-bold mx-2">
+              {t('job_detail_screen.apply_button')}
+            </Text>
           </TouchableOpacity>
         )}
       </View>
